92af2c820dc11a05ed7bc5250a5041c35d3c7388
Marcy Bursac <msbursac@gmail.com>
2018-09-26 20:17:08 -0500

    filters book display by shelf

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 /* jshint ignore:start */
 
 import React from 'react';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css';
 import MainPage from './MainPage';
 import SearchPage from './SearchPage';
 
 class BooksApp extends React.Component {
   state = {
-    showSearchPage: false
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   render() {
     return (
       <div className="app">
-        <MainPage />
-        {/* <SearchPage /> */}
+        <MainPage 
+          books={this.state.books}
+        />
       </div>
     )
   }
@@ -23,5 +30,5 @@ class BooksApp extends React.Component {
 
 export default BooksApp;
 
-/* written with help from tutorial from Maeva: */
+/* based on tutorial from Maeva: */
 /* https://www.youtube.com/watch?v=i6L2jLHV9j8 */
