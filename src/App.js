c65805da9cf50dde141a0bb3b6f4745e2181a63b
Marcy Bursac <msbursac@gmail.com>
2018-09-26 19:39:05 -0500

    creates Book.js

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {/* <MainPage /> */}
-        <SearchPage />
+        <MainPage />
+        {/* <SearchPage /> */}
       </div>
     )
   }
