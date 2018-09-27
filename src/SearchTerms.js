84e4147f264b7b8ff6c96c7a872d2c61f0c0dd0c
Marcy Bursac <msbursac@gmail.com>
2018-09-26 19:30:35 -0500

    styles search terms with asterixis

--- src/SearchTerms.js
+++ src/SearchTerms.js
@@ -6,18 +6,18 @@ class SearchTerms extends React.Component {
         return (
 
             <div className="search-terms">
-                Android, Art, Artificial Intelligence, Astronomy, Austen,
-                Baseball, Basketball, Bhagat, Biography, Brief, Business,
-                Camus, Cervantes, Christie, Classics, Comics, Cook, Cricket,
-                Cycling, Desai, Design, Development, Digital Marketing, Drama,
-                    Drawing, Dumas, Education, Everything, Fantasy, Film, Finance,
-                    First, Fitness, Football, Future, Games, Gandhi, Homer, Horror,
-                    Hugo, Ibsen, Journey, Kafka, King, Lahiri, Larsson, Learn,
-                    Literary Fiction, Make, Manage, Marquez, Money, Mystery, Negotiate,
-                        Painting, Philosophy, Photography, Poetry, Production, Programming,
-                        React, Redux, River, Robotics, Rowling, Satire, Science Fiction,
-                            Shakespeare, Singh, Swimming, Tale, Thrun, Time, Tolstoy, Travel,
-                            Ultimate, Virtual Reality, Web Development, iOS
+                Android * Art * Artificial Intelligence * Astronomy * Austen * 
+                Baseball * Basketball * Bhagat * Biography * Brief * Business *
+                Camus * Cervantes * Christie * Classics * Comics * Cook * Cricket *
+                Cycling * Desai * Design * Development * Digital Marketing * Drama *
+                    Drawing * Dumas * Education * Everything * Fantasy * Film * Finance *
+                    First * Fitness * Football * Future * Games * Gandhi * Homer * Horror *
+                    Hugo * Ibsen * Journey * Kafka * King * Lahiri * Larsson * Learn *
+                    Literary Fiction * Make * Manage * Marquez * Money * Mystery * Negotiate *
+                        Painting * Philosophy * Photography * Poetry * Production * Programming *
+                        React * Redux * River * Robotics * Rowling * Satire * Science Fiction *
+                            Shakespeare * Singh * Swimming * Tale * Thrun * Time * Tolstoy * Travel *
+                            Ultimate * irtual Reality * Web Development * iOS
                             </div >
 
         )
