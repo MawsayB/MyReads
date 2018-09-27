e8352cee5cb028ae91b655e0ef61bee11eafb8c1
Marcy Bursac <msbursac@gmail.com>
2018-09-27 12:55:41 -0500

    handles error case when searching, adds padding to search page to allow space for search terms library

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -22,10 +22,14 @@ class SearchPage extends React.Component {
   updateSearchedBooks = (query) => {
     if (query) {
       BooksAPI.search(query).then((searchedBooks) => {
-        this.setState({ searchedBooks: searchedBooks })
+        if (searchedBooks.error) {
+          this.setState({ searchedBooks: [] });
+        } else {
+          this.setState({ searchedBooks: searchedBooks })
+        }
       })
     } else {
-      this.setState({ searchedBook: [] });
+      this.setState({ searchedBooks: [] });
     }
   }
 
