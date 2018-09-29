429024125a726c46bfb6c86b0050b77767e2536c
Marcy Bursac <msbursac@gmail.com>
2018-09-28 19:05:23 -0500

    sets state correctly so book gets moved when a new shelf is selected

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class BooksApp extends React.Component {
     BooksAPI.update(book, shelf);
 
     BooksAPI.getAll().then((books) => {
-      this.setState({ books })
+      this.setState({ books: books })
     })
   }
 
